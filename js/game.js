aea04c2cab2ece81c98b6a49f06e3069c881666e
StephyStuff <stephanie.birch13@gmail.com>
2016-10-26 23:26:50 -0400

    Refactored version of dice-game. Created  border, font color, background color,and border radius variables. Corrected code at bottom of dice game to only show full year, full month, full date, hours and minutes.

--- js/game.js
+++ js/game.js
@@ -1,48 +1,51 @@
 (function diceGame() {
 
-console.log('start');
-    // Use document.querySelector('...') to find HTML elements
-    // The argument into the function is a CSS selector!
-    // Any HTML element has an `innerText` property that you can
-    // access (or change) for the contents of that element.
+  console.log('start');
+  // Use document.querySelector('...') to find HTML elements
+  // The argument into the function is a CSS selector!
+  // Any HTML element has an `innerText` property that you can
+  // access (or change) for the contents of that element.
 
-    document.querySelector('button').addEventListener('click', function buttonClicked() {
+  document.querySelector('button').addEventListener('click', function buttonClicked() {
 
-        // var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
-        // console.log(dieNumOne);
-        //
-        // var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
-        // console.log(dieNumTwo);
+    // var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
+    // console.log(dieNumOne);
+    //
+    // var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
+    // console.log(dieNumTwo);
 
-        var diceRollOne = diceGame.diceRoll();
-        var diceRollTwo = diceGame.diceRoll();
+    var diceRollOne = diceGame.diceRoll();
+    var diceRollTwo = diceGame.diceRoll();
 
-        document.querySelector('.dice aside:first-child').innerText = diceRollOne;
-        document.querySelector('.dice aside:last-child').innerText = diceRollTwo;
 
-        if ((diceRollOne + diceRollTwo) === 7 || (diceRollOne + diceRollTwo) === 11) {
-          document.querySelector('.result').innerText = 'Winner';
-          var finish = new Date();
-          document.querySelector('.message').innerText = '(It took you ' + ( diceGame.rounds.length + 1 ) + ' times and ' + ( (finish.getTime() - diceGame.startDate.getTime() ) / 1000) + ' seconds)';
-        }
-        else {
-          document.querySelector('.result').innerText = 'Try Again';
-        }
+    document.querySelector('.dice aside:first-child').innerText = diceRollOne;
+    document.querySelector('.dice aside:last-child').innerText = diceRollTwo;
 
-        diceGame.rounds.push (diceRollOne + diceRollTwo);
-          console.log(diceGame.rounds);
 
-    });
-
-    var diceGame = {
-      startDate: new Date(),
-      rounds: [],
-      diceRoll: function diceRoll() {
-        return Math.ceil(Math.random() * 6);
-      }
+    if ((diceRollOne + diceRollTwo) === 7 || (diceRollOne + diceRollTwo) === 11) {
+      document.querySelector('.result').innerText = 'Winner';
+      var finish = new Date();
+      document.querySelector('.message').innerText = '(It took you ' + ( diceGame.rounds.length + 1 ) + ' times and ' + ( (finish.getTime() - diceGame.startDate.getTime() ) / 1000) + ' seconds)';
+    }
+    else {
+      document.querySelector('.result').innerText = 'Try Again';
     }
 
+    diceGame.rounds.push (diceRollOne + diceRollTwo);
+    console.log(diceGame.rounds);
+
+  });
+
+  var diceGame = {
+    startDate: new Date(),
+    rounds: [],
+    diceRoll: function diceRoll() {
+      return Math.ceil(Math.random() * 6);
+    }
+
+  };
 
 
-document.querySelector('.game-start').innerText = 'Game started' + diceGame.startDate;
+
+  document.querySelector('.game-start').innerText = "Game started " + diceGame.startDate.getFullYear() + " " + (diceGame.startDate.getMonth()+1) + " " + diceGame.startDate.getDate() + " " + "at " + diceGame.startDate.getHours() + ":" + diceGame.startDate.getMinutes();
 })();
